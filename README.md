ae7c2069639099b43ff7d57cfdd9810e7d901302
hal117 <130006144+hal117@users.noreply.github.com>
2025-05-06 22:47:12 -0700

    added and passed unit tests

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ Make sure you make a PR to your own repo's main and not the class' repo!! Otherw
 Parts of Lab
 - [Expose Page](https://harrisonle128.github.io/Lab5_Starter/expose.html)
 - [Explore Page](https://harrisonle128.github.io/Lab5_Starter/explore.html)
+
+## Checking Understanding
+1. Would you use a unit test to test the “message” feature of a messaging application? Why or why not? 
+No, I would not use a unit test for this feature because it involves more complex interactions such as sending data over a network. These are better tested using end-to-end tests.
+
+
+1. Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not?
+Yes, I would use a unit test because checking the length of a message is a simple logic that can be tested knowing what the output and input should be. Unit tests are great under this condition. 
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -9,3 +9,71 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+test('valid phone number: 123-456-7890', () => {
+  expect(isPhoneNumber('123-456-7890')).toBe(true);
+});
+test('valid phone number: (555) 555-5555', () => {
+  expect(isPhoneNumber('(555) 555-5555')).toBe(true);
+});
+test('invalid phone number: 123456', () => {
+  expect(isPhoneNumber('123456')).toBe(false);
+});
+test('invalid phone number: abc-def-ghij', () => {
+  expect(isPhoneNumber('abc-def-ghij')).toBe(false);
+});
+
+// isEmail
+test('valid email: example@email.com', () => {
+  expect(isEmail('example@email.com')).toBe(true);
+});
+test('valid email: abc@xyz.com', () => {
+  expect(isEmail('abc@xyz.com')).toBe(true);
+});
+test('invalid email: justtext', () => {
+  expect(isEmail('justtext')).toBe(false);
+});
+test('invalid email: noatsign.com', () => {
+  expect(isEmail('noatsign.com')).toBe(false);
+});
+
+// isStrongPassword
+test('valid password: Abc1234', () => {
+  expect(isStrongPassword('Abc1234')).toBe(true);
+});
+test('valid password: pass_word1', () => {
+  expect(isStrongPassword('pass_word1')).toBe(true);
+});
+test('invalid password: 123', () => {
+  expect(isStrongPassword('123')).toBe(false);
+});
+test('invalid password: @@@@', () => {
+  expect(isStrongPassword('@@@@')).toBe(false);
+});
+
+// isDate
+test('valid date: 01/01/2020', () => {
+  expect(isDate('01/01/2020')).toBe(true);
+});
+test('valid date: 12/31/1999', () => {
+  expect(isDate('12/31/1999')).toBe(true);
+});
+test('invalid date: 9999/12/31', () => {
+  expect(isDate('9999/12/31')).toBe(false); 
+});
+test('invalid date: 2020-01-01', () => {
+  expect(isDate('2020-01-01')).toBe(false);
+});
+
+// isHexColor
+test('valid hex: #FFFFFF', () => {
+  expect(isHexColor('#FFFFFF')).toBe(true);
+});
+test('valid hex: #000', () => {
+  expect(isHexColor('#000')).toBe(true);
+});
+test('invalid hex: 12345', () => {
+  expect(isHexColor('12345')).toBe(false);
+});
+test('invalid hex: #ZZZZZZ', () => {
+  expect(isHexColor('#ZZZZZZ')).toBe(false);
+});
